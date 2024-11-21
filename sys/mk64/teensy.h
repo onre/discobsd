607cb9e459235141001d82f90a3b9b5f8aa5db6e
Erno <esp@iki.fi>
2024-11-21 06:35:31 +0200

    this state seems to work, too

--- sys/mk64/teensy.h
+++ sys/mk64/teensy.h
@@ -1,6 +1,12 @@
 #ifndef _MACHINE_TEENSY_H_
 #define _MACHINE_TEENSY_H_
 
+extern unsigned long _edata;
+extern unsigned long _etext;
+extern void (*_VectorsRam[102])(void);
+extern unsigned long _sdata;
+
+
 #ifdef TEENSY35
 
 #        define TEENSY_LED_PIN_MASK (1 << 5)
