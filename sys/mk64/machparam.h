5b0c088ecd1ef80c5a1e715fe2239dcb80d42afa
Erno <esp@iki.fi>
2024-11-22 08:18:46 +0200

    a sanity check

--- sys/mk64/machparam.h
+++ sys/mk64/machparam.h
@@ -1,10 +1,15 @@
 /*
- * Machine dependent constants for STM32.
+ * Machine dependent constants for Kinetis
  *
  * Copyright (c) 1986 Regents of the University of California.
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  */
+
+#if defined(USB_DISABLED) && defined(USB_SERIAL)
+#error can not define USB_DISABLED and USB_SERIAL simultaneously
+#endif
+
 #ifndef ENDIAN
 
 #    define MACHINE "mk64"
